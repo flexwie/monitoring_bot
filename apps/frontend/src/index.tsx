c3123ad2419aa940ff6f297a5e9ff1e7318aa1b8
Felix Wieland <mail@felixwie.com>
2022-12-11 11:48:42 +0100

    added tailwind

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -4,10 +4,12 @@ import 'core-js/features/promise';
 import 'core-js/features/set';
 import 'raf/polyfill';
 import 'whatwg-fetch';
+import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
 
 const container = document.getElementById('app-root')!;
 
-ReactDOM.render(<h1>Hi</h1>, container);
+ReactDOM.render(<App />, container);
